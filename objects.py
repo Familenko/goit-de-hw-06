8b4f67d6c3b076a27ba53f243d79fdd06864ecf8
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2025-08-26 10:13:03 +0300

    working stream

---
+++ objects.py
@@ -0,0 +1,46 @@
+from kafka import KafkaConsumer, KafkaProducer
+from kafka.admin import KafkaAdminClient
+from configs import kafka_config
+import json
+
+
+# Створення клієнта Kafka
+admin_client = KafkaAdminClient(
+    bootstrap_servers=kafka_config['bootstrap_servers'],
+    security_protocol=kafka_config['security_protocol'],
+    sasl_mechanism=kafka_config['sasl_mechanism'],
+    sasl_plain_username=kafka_config['username'],
+    sasl_plain_password=kafka_config['password']
+)
+
+# Створення Kafka Consumer
+def make_consumer():
+    consumer = KafkaConsumer(
+        bootstrap_servers=kafka_config['bootstrap_servers'],
+        security_protocol=kafka_config['security_protocol'],
+        sasl_mechanism=kafka_config['sasl_mechanism'],
+        sasl_plain_username=kafka_config['username'],
+        sasl_plain_password=kafka_config['password'],
+        value_deserializer=lambda v: json.loads(v.decode('utf-8')),
+        key_deserializer=lambda v: json.loads(v.decode('utf-8')),
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,       
+        group_id='my_consumer_group_3',
+        consumer_timeout_ms=3_000
+    )
+
+    return consumer
+
+# Створення Kafka Producer
+def make_producer():
+    producer = KafkaProducer(
+        bootstrap_servers=kafka_config['bootstrap_servers'],
+        security_protocol=kafka_config['security_protocol'],
+        sasl_mechanism=kafka_config['sasl_mechanism'],
+        sasl_plain_username=kafka_config['username'],
+        sasl_plain_password=kafka_config['password'],
+        value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+        key_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
+
+    return producer
